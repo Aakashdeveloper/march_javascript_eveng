49e6f9f2128fd998bfe3ad75b9a56aecaba3fc61
aakashdeveloper <ahanda205@gmail.com>
2021-03-08 19:36:25 +0530

    d

---
+++ 2dataType.js
@@ -0,0 +1,115 @@
+var a = true
+var b= true
+a+b
+2
+true+false
+1
+10+true
+11
+true+10
+11
+10/false
+Infinity
+
+true is equal to 1
+false is equal to 0
+
+
+"a"+true
+"atrue"
+true+"a"
+"truea"
+true+10
+11
+false+"a"
+"falsea"
+
+"true"+"true"
+"truetrue"
+
+var a = "10"
+var b = "20"
+a+b
+
+var a = "10"
+var b = "20"
+a+b
+"1020"
+parseInt(a)
+10
+var c= "10a"
+undefined
+parseInt(a)
+10
+var c= "10a10"
+undefined
+parseInt(a)
+10
+var b = "a20"
+undefined
+parseInt(b)
+NaN
+var b = "10101233a20"
+undefined
+parseInt(b)
+10101233
+var c= "hi"
+undefined
+parseInt(c)
+NaN
+
+var a = "10"
+var b = "20"
+undefined
+parseInt(a)+parseInt(b)
+30
+var c = "10.33"
+undefined
+var d = "20.11"
+undefined
+parseInt(c)+parseInt(d)
+30
+parseFloat(a)+parseFloat(b)
+30
+parseFloat(c)+parseFloat(d)
+30.439999999999998
+
+
+var a = "10"
+var b = "20"
+undefined
+parseInt(a)+parseInt(b)
+30
+var c = "10.33"
+undefined
+var d = "20.11"
+undefined
+parseInt(c)+parseInt(d)
+30
+parseFloat(a)+parseFloat(b)
+30
+parseFloat(c)+parseFloat(d)
+30.439999999999998
+Number(c)+Number(d)
+30.439999999999998
+Number(a)+Number(b)
+30
+Number(a)+Number(c)
+20.33
+
+var a = "10"
+undefined
+var b = 10
+undefined
+var c ="Hi"
+undefined
+isNaN(a)
+false
+isNaN(b)
+false
+isNaN(c)
+true
+var d = "10a"
+undefined
+isNaN(d)
+true
---
+++ 3oprators.js
@@ -0,0 +1,66 @@
+= assignment
+== compare the data
+=== compare the data and datatype
+
+var a = 20
+undefined
+var b = "20"
+undefined
+a==b  (comapre value as both side it is 20)
+true
+a===b (comapre value as both side it is 20 but datatype is different)
+false
+
+a>10
+a<20
+a>=10
+a<=20
+a!==b
+a!=b
+
+all of then will return boolean response
+
+var a = 20
+undefined
+var b = "20"
+undefined
+a==b
+true
+a===b
+false
+a>10
+true
+a<10
+false
+a!==b
+true
+a!=b
+false
+
+var a = true
+!a
+false
+
+var b = false
+!b
+true
+
+var a = 1
+!a
+false
+
+var b = -1
+!b
+false
+
+var c = 0
+!c
+true
+
+
+truthy = any number except 0 weather +ve or -ve / string /true
+falsy = 0,null,undefined, false
+
+var a = "hi"
+!a
+false
